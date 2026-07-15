0e2ac34bf0a7c041d041d2ee18083ee50167658e
collinc254 <nduchezcollins@gmail.com>
2026-07-15 17:12:23 +0300

    Fixed controller and updated properties

---
+++ middleware/src/main/java/com/openfloat/middleware/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.openfloat.middleware.controller;
+
+import com.openfloat.middleware.dto.DarajaStkPushResponse;
+import com.openfloat.middleware.dto.StkPushRequest;
+import com.openfloat.middleware.service.StkPushService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final StkPushService stkPushService;
+
+    @PostMapping("/trigger")
+    public ResponseEntity<DarajaStkPushResponse> triggerPayment(@RequestBody StkPushRequest request) {
+        DarajaStkPushResponse response = stkPushService.sendPush(request);
+        return ResponseEntity.ok(response);
+    }
+}
