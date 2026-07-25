12b2eef88d62315accf8a836082052939792471e
collinc254 <nduchezcollins@gmail.com>
2026-07-25 14:42:41 +0300

    fix: enforce json property capitalization for daraja payload

--- middleware/src/main/java/com/openfloat/middleware/dto/B2CRequest.java
+++ middleware/src/main/java/com/openfloat/middleware/dto/B2CRequest.java
@@ -1,19 +1,40 @@
 package com.openfloat.middleware.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class B2CRequest {
+
+    @JsonProperty("InitiatorName")
     private String InitiatorName;
+
+    @JsonProperty("SecurityCredential")
     private String SecurityCredential;
-    private String CommandID; 
+
+    @JsonProperty("CommandID")
+    private String CommandID;
+
+    @JsonProperty("Amount")
     private String Amount;
+
+    @JsonProperty("PartyA")
     private String PartyA;
+
+    @JsonProperty("PartyB")
     private String PartyB;
+
+    @JsonProperty("Remarks")
     private String Remarks;
+
+    @JsonProperty("QueueTimeOutURL")
     private String QueueTimeOutURL;
+
+    @JsonProperty("ResultURL")
     private String ResultURL;
+
+    @JsonProperty("Occasion")
     private String Occasion;
 }
